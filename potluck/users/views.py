88a896aeacc2dcf1be7456e94c7d6b14b5053c90
Henry Doan <henrydoan96@gmail.com>
2021-11-14 23:39:22 -0700

    semantic and start auth

---
+++ potluck/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      first_name = form.cleaned_data.get('first_name')
+      last_name = form.cleaned_data.get('last_name')
+      messages.success(request, f'Welcome {first_name} {last_name}! You are logged in!')
+      return redirect('login')
+  else:
+    form = RegistrationForm()
+  return render(request, 'users/register.html', {'form': form})
+
+# @login_required
