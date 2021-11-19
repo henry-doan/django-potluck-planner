22d94bb017a964f6e3e8c05f9711a6c699f996ae
Henry Doan <henrydoan96@gmail.com>
2021-11-18 19:50:32 -0700

    models

--- potluck/planner/admin.py
+++ potluck/planner/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Contact, Event, Item
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Event)
+admin.site.register(Item)
--- potluck/planner/models.py
+++ potluck/planner/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+  def __str__(self):
+    return self.email
+
+  name = models.CharField(max_length=200)
+  email = models.CharField(max_length=200)
+  msg = models.TextField(max_length=2000)
+
+
+class Event(models.Model):
+  def __str__(self):
+    return self.name
+
+  name = models.CharField(max_length=200)
+  desc = models.TextField(max_length=2000)
+  image = models.ImageField(upload_to='pictures', default='pictures/none/noimg.jpg')
+  location = models.CharField(max_length=200)
+  start_day = models.DateField()
+  end_day = models.DateField()	
+  start_time = models.TimeField()
+  end_time = models.TimeField()	
+
+
+class Item(models.Model):
+  def __str__(self):
+    return self.name
+
+  name = models.CharField(max_length=200)
+  category = models.CharField(max_length=200)
+  userId = models.IntegerField()	
+  image = models.ImageField(upload_to='pictures', default='pictures/none/noimg.jpg')
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
