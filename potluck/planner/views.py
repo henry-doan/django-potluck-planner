0a900e9696f78e48dd24f954be61f045a433dede
Henry Doan <henrydoan96@gmail.com>
2021-11-18 21:19:39 -0700

    contact done and style forms

--- potluck/planner/views.py
+++ potluck/planner/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
   return render(request, 'planner/home.html')
 
 def contact(request):
-  return render(request, 'planner/contact.html')
+  if request.method == 'POST':
+    name = request.POST.get('name', '')
+    email = request.POST.get('email', '')
+    msg = request.POST.get('msg', '')
+    contact = Contact(name=name, email=email, msg=msg)
+    
+    contact.save()
+  return render(request, 'planner/home.html')
+  # return render(request, 'planner/thank-you.html')
