2334ac364b1f28275abf7d35ab56b1e964d3b64a
Henry Doan <henrydoan96@gmail.com>
2021-11-24 09:36:05 -0700

    register and login working properly

--- potluck/users/views.py
+++ potluck/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
@@ -13,10 +14,14 @@ def register(request):
       first_name = form.cleaned_data.get('first_name')
       last_name = form.cleaned_data.get('last_name')
       messages.success(request, f'Welcome {first_name} {last_name}! You are logged in!')
-      return redirect('login')
-    else:
-      print("not valid")
-      print(form)
+      new_user = authenticate(username=form.cleaned_data['username'],
+                              password=form.cleaned_data['password1'],
+                              )
+      login(request, new_user)
+      return redirect('events')
+    # else:
+    #   print("not valid")
+    #   print(form)
   else:
     form = RegistrationForm()
   return render(request, 'users/register.html', {'form': form})
